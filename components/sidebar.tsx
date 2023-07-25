d6a9cccaba8edb2917798bd347e16323e5ba1d05
Matthew Fogarty <mattempyre@gmail.com>
2023-07-25 20:06:14 +0200

    added conditional sltyling to nav links

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import { Montserrat } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import {
@@ -61,6 +62,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -72,12 +74,17 @@ const Sidebar = () => {
             Genius
           </h1>
         </Link>
-        <div className="space-y-1">
+        <nav className="space-y-1">
           {routes.map((route) => (
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                pathname === route.href
+                  ? 'text-white bg-white/10'
+                  : 'text-zinc-400'
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
@@ -85,7 +92,7 @@ const Sidebar = () => {
               </div>
             </Link>
           ))}
-        </div>
+        </nav>
       </div>
     </div>
   );
