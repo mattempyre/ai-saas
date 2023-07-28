2b4f756f07d52660200072413538ae18457669ab
Matthew Fogarty <mattempyre@gmail.com>
2023-07-28 20:33:57 +0200

    added toast error messages

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -21,6 +21,7 @@ import { UserAvatar } from '@/components/user-avatar';
 import { BotAvatar } from '@/components/bot-avatar';
 import ReactMarkdown from 'react-markdown';
 import { useProModal } from '@/hooks/use-pro-modal';
+import toast from 'react-hot-toast';
 
 const CodePage = () => {
   const proModal = useProModal();
@@ -52,8 +53,9 @@ const CodePage = () => {
     } catch (error: any) {
       if (error?.response?.status === 403) {
         proModal.onOpen();
+      } else {
+        toast.error('Something went wrong');
       }
-      console.log(error);
     } finally {
       router.refresh();
     }
